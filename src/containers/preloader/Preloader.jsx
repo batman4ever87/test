d62f7b5ed5348f8753f752a338233dc9368e36a1
Johannes Kujanpää <b61648764@gmail.com>
2024-07-13 12:31:34 +0300

    Initial commit

--- src/containers/preloader/Preloader.jsx
+++ src/containers/preloader/Preloader.jsx
@@ -7,7 +7,8 @@ gsap.registerPlugin(useGSAP);
 const Preloader = () => {
     useGSAP(() => {
         gsap.from("h2", {yPercent: -100})
-        gsap.fromTo("h1 span", {rotate: 20, opacity: 0, yPercent: 60}, {rotate: 0, stagger: 0.1, opacity: 1, yPercent: 0, delay: .3})
+        gsap.fromTo(".up", {yPercent: -50, opacity: 0}, {yPercent: 0, opacity: 1, delay: .5})
+        gsap.fromTo(".bot", {yPercent: 50, opacity: 0}, {yPercent: 0, opacity: 1, delay: .5})
         gsap.fromTo(".line", {width: "0%"}, {width: "90%", delay: 1.4})
         gsap.fromTo(".cta", {filter: "blur(10px)"}, {filter: "blur(0px)", delay: 2})
     }, [])
@@ -18,8 +19,8 @@ const Preloader = () => {
             <h2>Ansioluettelo</h2>
         </div>
         <div className="preloader-content">
-            <h1><span>J</span><span>a</span><span>s</span><span>o</span><span>n</span></h1>
-            <h1><span>w</span><span>i</span><span>l</span><span>l</span><span>i</span><span>a</span><span>m</span><span>s</span></h1>
+            <h1 className="up">Jason</h1>
+            <h1 className="bot">Williams</h1>
             <div className="line"></div>
             <button className="cta">Enter</button>
         </div>
